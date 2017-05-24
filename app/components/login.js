a2e3be51c6a9779e7893021d8d9d5461dc8236ae
Britt Verschueren <brittverschueren1996@hotmail.com>
2017-05-24 22:03:06 +0200

    svg

--- app/components/login.js
+++ app/components/login.js
@@ -3,6 +3,16 @@ zoeff.components = zoeff.components || {};
 (function (app) {
     app.components.login = Vue.component('login', {
         template: '#login',
+        beforeMount: function () {
+            var body = document.getElementsByTagName('body')[0];
+            console.log(body);
+            body.classList.add('login');
+        },
+         beforeDestroy: function () {
+            var body = document.getElementsByTagName('body')[0];
+            console.log(body);
+            body.classList.remove('login');
+        },
         methods: {
             login: function (ev) {
                 ev.preventDefault();
@@ -15,12 +25,21 @@ zoeff.components = zoeff.components || {};
                 var db = app.firebaseApp.database();
                 db.ref('users/' + data.username).once('value').then(
                     function (snapshot) {
-                        var pw = snapshot.val().password;
-                        if (pw === data.password) {
-                            app.router.push('/my');
-                        } else {
-                            alert('niet aangemeld');
+                        var val = snapshot.val();
+                        if (!val) {
+                            alert('Gebruikersnaam of wachtwoord fout');
+
                         }
+                        else {
+                            var pw = val.password;
+
+                            if (pw === data.password) {
+                                app.router.push('/my');
+                            } else {
+                                alert('Gebruikersnaam of wachtwoord fout');
+                            }
+                        }
+
                     }
                 )
             }
