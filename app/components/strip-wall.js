358ebe13ecc340922bd3f3d120170ac16404ada8
Britt Verschueren <brittverschueren1996@hotmail.com>
2017-05-30 21:02:58 +0200

    wall styling fix + internet explorer fix

--- app/components/strip-wall.js
+++ app/components/strip-wall.js
@@ -2,10 +2,15 @@
     attachclicks = function (el, strips, clickstriphandler) {
         _.forEach(strips, function (strip, index) {
             var className = 'strip-' + (index + 1);
-            var svg = _.find(el.children, function (el) { return el.tagName === 'svg'; });
-            var children = _.filter(svg.children, function (el) {
-                return (' ' + el.className.baseVal + ' ').indexOf(' ' + className + ' ') > -1;
+            var svg = _.find(el.children, function (el) {
+                return el.tagName === 'svg';
             });
+            var children = _.filter(svg.childNodes, function (el) {
+                return el &&
+                    el.className &&
+                    (' ' + el.className.baseVal + ' ').indexOf(' ' + className + ' ') > -1;
+            });
+
             _.forEach(children, function (child) {
                 child.onclick = function () {
                     clickstriphandler(strip);
--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,29 @@
 var zoeff = zoeff || {};
 (function (app) {
-    var routes = [
-        { path: '/', redirect: '/login' },
-        { path: '/my', component: app.components.myStrips },
-        { path: '/add', component: app.components.allStrips },
-        { path: '/login', component: app.components.login },
-        { path: '/register', component: app.components.register },
-        { path: '/quiz', component: app.components.quiz },
+    var routes = [{
+            path: '/',
+            redirect: '/login'
+        },
+        {
+            path: '/my',
+            component: app.components.myStrips
+        },
+        {
+            path: '/add',
+            component: app.components.allStrips
+        },
+        {
+            path: '/login',
+            component: app.components.login
+        },
+        {
+            path: '/register',
+            component: app.components.register
+        },
+        {
+            path: '/quiz',
+            component: app.components.quiz
+        },
     ]
 
     var router = new VueRouter({
@@ -16,9 +33,8 @@ var zoeff = zoeff || {};
     app.router = router;
 
     var vueApp = new Vue({
-        router
+        router: router,
     }).$mount('#app');
 
     Vue.use(VueFire);
 })(zoeff);
-
